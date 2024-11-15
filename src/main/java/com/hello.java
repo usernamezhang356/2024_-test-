3b9a6e43ba13bd0527c62a629bae639a63c3b0cd
Zyijie <347588831@qq.com>
2024-11-15 12:51:58 +0800

    diyici

---
+++ src/main/java/com/hello.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @Version :1.0
+ * @Author :张義杰
+ * @File :hello.py
+ * @Time :2024/11/15 12:50
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("heelo");
+    }
+}
